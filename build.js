eb46ae9810e815d713338d0095ab18ed352f15d1
Titus Wormer <tituswormer@gmail.com>
2016-11-07 15:42:34 +0100

    Replace cheerio with jsdom

--- build.js
+++ build.js
@@ -2,20 +2,9 @@
 
 /* Dependencies. */
 var fs = require('fs');
-var http = require('http');
-var https = require('https');
-var cheerio = require('cheerio');
+var jsdom = require('jsdom');
 var bail = require('bail');
 var list = require('./');
-
-/* Constants. */
-var HTTPS = 'https://';
-var PREFIX = 'elements-3:';
-
-/* Input / output locations. */
-var w3c = 'http://w3c.github.io/elements-of-html/';
-var whatwg = 'https://html.spec.whatwg.org/multipage/indices.html#elements-3';
-var output = 'index.json';
 
 var count = 0;
 
@@ -24,16 +13,18 @@ function done() {
   count++;
 
   if (count === 2) {
-    fs.writeFile(output, JSON.stringify(list.sort(), 0, 2) + '\n', bail);
+    fs.writeFile('index.json', JSON.stringify(list.sort(), 0, 2) + '\n', bail);
   }
 }
 
 /* Crawl W3C. */
-load(w3c, function (err, doc) {
+jsdom.env('http://w3c.github.io/elements-of-html/', function (err, window) {
   bail(err);
 
-  cheerio.load(doc)('[scope="row"] code').each(function () {
-    var data = this.children[0].data;
+  var rows = [].slice.call(window.document.querySelectorAll('[scope="row"] code'));
+
+  rows.forEach(function (row) {
+    var data = row.textContent;
 
     if (data && !/\s/.test(data) && list.indexOf(data) === -1) {
       list.push(data);
@@ -44,42 +35,22 @@ load(w3c, function (err, doc) {
 });
 
 /* Crawl WHATWG. */
-load(whatwg, function (err, doc) {
+jsdom.env('https://html.spec.whatwg.org/multipage/indices.html#elements-3', function (err, window) {
   bail(err);
 
-  cheerio
-    .load(doc)('tbody th code')
-    .each(function () {
-      var id = this.attribs.id;
-      var child = this.children[0].children;
-      var data = child && child[0] && child[0].data;
+  var rows = [].slice.call(window.document.querySelectorAll('tbody th code'));
 
-      if (
-        id &&
-        id.slice(0, PREFIX.length) === PREFIX &&
-        list.indexOf(data) === -1
-      ) {
-        list.push(data);
-      }
-    });
+  rows.forEach(function (row) {
+    var data = row.children && row.children[0] && row.children[0].textContent;
+
+    if (
+      row.id &&
+      row.id.slice(0, 'elements-3:'.length) === 'elements-3:' &&
+      list.indexOf(data) === -1
+    ) {
+      list.push(data);
+    }
+  });
 
   done();
 });
-
-/* Load. */
-function load(url, callback) {
-  var lib = url.slice(0, HTTPS.length) === HTTPS ? https : http;
-
-  lib.get(url, function (res) {
-    var value = '';
-    res
-      .setEncoding('utf8')
-      .on('data', function (buf) {
-        value += buf;
-      })
-      .on('end', function () {
-        return callback(null, value);
-      })
-      .on('error', callback);
-  });
-}
