bfa3f8c81a2f23d3980d3d25824a7fbdeba0632e
Titus Wormer <tituswormer@gmail.com>
2018-04-30 13:21:48 +0200

    Refactor code-style

--- build.js
+++ build.js
@@ -1,60 +1,70 @@
-'use strict';
+'use strict'
 
-var fs = require('fs');
-var https = require('https');
-var concat = require('concat-stream');
-var bail = require('bail');
-var unified = require('unified');
-var html = require('rehype-parse');
-var selectAll = require('hast-util-select').selectAll;
-var toString = require('hast-util-to-string');
-var list = require('.');
+var fs = require('fs')
+var https = require('https')
+var concat = require('concat-stream')
+var bail = require('bail')
+var unified = require('unified')
+var html = require('rehype-parse')
+var selectAll = require('hast-util-select').selectAll
+var toString = require('hast-util-to-string')
+var list = require('.')
 
-var count = 0;
+var proc = unified().use(html)
 
-function done() {
-  count++;
+var count = 0
 
-  if (count === 2) {
-    fs.writeFile('index.json', JSON.stringify(list.sort(), 0, 2) + '\n', bail);
+/* Crawl W3C. */
+https.get('https://w3c.github.io/elements-of-html/', onw3c)
+
+/* Crawl WHATWG. */
+https.get('https://html.spec.whatwg.org/multipage/indices.html', onwhatwg)
+
+function onw3c(res) {
+  res.pipe(concat(onconcat)).on('error', bail)
+
+  function onconcat(buf) {
+    selectAll('[scope="row"] code', proc.parse(buf)).forEach(each)
+
+    done()
+  }
+
+  function each(node) {
+    var data = toString(node)
+
+    if (data && !/\s/.test(data) && list.indexOf(data) === -1) {
+      list.push(data)
+    }
   }
 }
 
-/* Crawl W3C. */
-https.get('https://w3c.github.io/elements-of-html/', function (res) {
-  res.pipe(concat(onconcat)).on('error', bail);
+function onwhatwg(res) {
+  res.pipe(concat(onconcat)).on('error', bail)
 
   function onconcat(buf) {
-    selectAll('[scope="row"] code', unified().use(html).parse(buf)).forEach(each);
+    selectAll('tbody th code', proc.parse(buf)).forEach(each)
 
-    done();
+    done()
+  }
 
-    function each(node) {
-      var data = toString(node);
+  function each(node) {
+    var id = node.properties.id
+    var data = toString(node)
 
-      if (data && !/\s/.test(data) && list.indexOf(data) === -1) {
-        list.push(data);
-      }
+    if (
+      id &&
+      id.slice(0, 'elements-3:'.length) === 'elements-3:' &&
+      list.indexOf(data) === -1
+    ) {
+      list.push(data)
     }
   }
-});
+}
 
-/* Crawl WHATWG. */
-https.get('https://html.spec.whatwg.org/multipage/indices.html', function (res) {
-  res.pipe(concat(onconcat)).on('error', bail);
+function done() {
+  count++
 
-  function onconcat(buf) {
-    selectAll('tbody th code', unified().use(html).parse(buf)).forEach(each);
-
-    done();
-
-    function each(node) {
-      var id = node.properties.id;
-      var data = toString(node);
-
-      if (id && id.slice(0, 'elements-3:'.length) === 'elements-3:' && list.indexOf(data) === -1) {
-        list.push(data);
-      }
-    }
+  if (count === 2) {
+    fs.writeFile('index.json', JSON.stringify(list.sort(), 0, 2) + '\n', bail)
   }
-});
+}
--- test.js
+++ test.js
@@ -1,21 +1,14 @@
-'use strict';
+'use strict'
 
-var test = require('tape');
-var htmlTagNames = require('.');
+var test = require('tape')
+var htmlTagNames = require('.')
 
-test('htmlTagNames', function (t) {
-  t.ok(
-    Array.isArray(htmlTagNames),
-    'should be an `array`'
-  );
+test('htmlTagNames', function(t) {
+  t.ok(Array.isArray(htmlTagNames), 'should be an `array`')
 
-  htmlTagNames.forEach(function (tagName) {
-    t.equal(
-      typeof tagName,
-      'string',
-      '`' + tagName + '` should be a string'
-    );
-  });
+  htmlTagNames.forEach(function(tagName) {
+    t.equal(typeof tagName, 'string', '`' + tagName + '` should be a string')
+  })
 
-  t.end();
-});
+  t.end()
+})
