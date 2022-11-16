fa3272a962007a05d3971776855ab6ca8a35147b
Titus Wormer <tituswormer@gmail.com>
2022-11-16 11:35:28 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,18 +1,17 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {htmlTagNames} from './index.js'
 
-test('htmlTagNames', function (t) {
+test('htmlTagNames', function () {
   let index = -1
 
-  t.ok(Array.isArray(htmlTagNames), 'should be an `array`')
+  assert.ok(Array.isArray(htmlTagNames), 'should be an `array`')
 
   while (++index < htmlTagNames.length) {
-    t.equal(
+    assert.equal(
       typeof htmlTagNames[index],
       'string',
       '`' + htmlTagNames[index] + '` should be a string'
     )
   }
-
-  t.end()
 })
